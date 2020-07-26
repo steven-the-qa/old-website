124a8ad61e2813f4960d8fc5931085194c76b3d0
Steven <boutchersj@gmail.com>
2020-07-26 12:15:16 -0500

    Initial commit - just the Express app

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/blog', (req, res) => {
+    res.send('Hello Blog')
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {console.log(`Now listening on port ${port}`)})
