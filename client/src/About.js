2d027cbbf4df5e64037d280924ac48eb08c5182c
Steven <boutchersj@gmail.com>
2020-08-29 16:15:19 -0500

    trying out a scale property to adjust zoom on mobile

--- client/src/About.js
+++ client/src/About.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Helmet } from 'react-helmet'
 import Navbar from './Navbar'
 import Header from './Header'
@@ -7,29 +7,29 @@ import Hobbies from './Hobbies'
 import Education from './Education'
 
 
-class About extends React.Component {
-    constructor() {
-        super()
-        this.state = {
+function About() {
+    const [width, setWidth] = useState(window.innerWidth);
 
-        }
-    }
+    useEffect(() => {
+        const handleWindowResize = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleWindowResize);
+        return () => window.removeEventListener("resize", handleWindowResize);
+    }, []);
 
-    render() {
-
-        return (
-            <div className="About">
-                <Helmet>
-                    <title>About | Steven Boutcher</title>
-                </Helmet>
-                <Navbar currentPage="About" />
-                <Header title="STEVEN BOUTCHER" subtitle="Self-Taught Full Stack Developer" />
-                <Bio />
-                <Hobbies />
-                <Education />
-            </div>
-        );
-    }
+    const breakpoint = 640
+    const adjustScale = width > breakpoint ? { 'transform': 'scale(1)' } : { 'transform': 'scale(.8)' }
+    return (
+        <div className="About" style={adjustScale}>
+            <Helmet>
+                <title>About | Steven Boutcher</title>
+            </Helmet>
+            <Navbar currentPage="About" />
+            <Header title="STEVEN BOUTCHER" subtitle="Self-Taught Full Stack Developer" />
+            <Bio />
+            <Hobbies />
+            <Education />
+        </div>
+    );
 }
 
 export default About
