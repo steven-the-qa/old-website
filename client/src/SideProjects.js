7daf2622e9df1b79e8dba62ac62f862ad5d93a09
Steven <boutchersj@gmail.com>
2020-09-08 08:45:45 -0500

    Attemtping to fix the horizontal overflow of the Publications and Side Projects images; created the description side of the Side Projects card

--- client/src/SideProjects.js
+++ client/src/SideProjects.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import sideProjects from './sideProjects.json'
 
 export default function SideProjects() {
+    const [cardSide, setCardSide] = useState('front');
+    const flipCard = () => {
+        cardSide === 'front' ? setCardSide('reverse') : setCardSide('front')
+    }
+    const FrontSide = () => {
+        return(
+            <div id="medicareandmore" className='sideProject' onClick={flipCard}>
+                Medicare & More Insurance Agency
+            </div>
+        )
+    }
+    const features = sideProjects.medicareandmore.features.map(feature => {
+        return (
+            <li className='sideProject-feature'>{feature}</li>
+        )
+    })
 
-    return (
+    const ReverseSide = () => {
+        return (
+            <div id="medicareandmore-description" className='sideProject-description' onClick={flipCard}>
+                <h2>{sideProjects.medicareandmore.title}</h2>
+                <strong>Description:</strong>
+                <p>{sideProjects.medicareandmore.description}</p>
+                <strong>Features:</strong>
+                <ul>{features}</ul>
+                <a className='btn-repo' href={sideProjects.medicareandmore.repo}>View Repo</a>
+                <a className='btn-repo' href={sideProjects.medicareandmore.live}>View Live Site</a>
+            </div>
+        )
+    }
+
+    return(
         <div className='sideProjects'>
             <h3 className='sideProjects-title'>Side Projects</h3>
-            <a id="medicareandmore" className='sideProject' href="https://medicaremoreagency.com/" target="_blank" rel="noopener noreferrer">
-                Medicare & More Insurance Agency
-            </a>
+            {cardSide === 'front' ? <FrontSide /> : <ReverseSide />}
         </div>
     )
+    
 }
