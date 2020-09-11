0c7d68f4c60b5aa3ac7343fe21db72c6bcfcb9e1
Steven <boutchersj@gmail.com>
2020-09-11 09:16:09 -0500

    Fixed a resizing issue on the Hobbies section; applied the same CSS to the Public Profiles section

--- client/src/PublicProfiles.js
+++ client/src/PublicProfiles.js
@@ -4,18 +4,13 @@ export default function Hobbies() {
 
     return (
         <div className='publicProfiles'>
-            <div className="publicProfileDiv">
-                <a id="github" className="profileLink" href="https://github.com/boutchersj" target="_blank" rel="noopener noreferrer" alt="GitHub profile link">
-                    <div className="profileLinkDiv">GitHub Logo Image</div>
-                </a>
-            </div>
+            <a id="github" className="profileLink" href="https://github.com/boutchersj" target="_blank" rel="noopener noreferrer" alt="GitHub profile link">
+                <div className="profileLinkDiv">GitHub Logo Image</div>
+            </a>
 
-            <div className="publicProfileDiv">
-                <a id="freecodecamp" className="profileLink" href="https://www.freecodecamp.org/boutchersj" target="_blank" rel="noopener noreferrer" alt="freeCodeCamp profile link">
-                    <div className="profileLinkDiv">freeCodeCamp Logo Image</div>
-                </a>
-            </div>
-
+            <a id="freecodecamp" className="profileLink" href="https://www.freecodecamp.org/boutchersj" target="_blank" rel="noopener noreferrer" alt="freeCodeCamp profile link">
+                <div className="profileLinkDiv">freeCodeCamp Logo Image</div>
+            </a>
         </div>
     )
 }
