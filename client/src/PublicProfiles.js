b8829d08476b9422ea3f31ac4581cb87aade106c
Steven <boutchersj@gmail.com>
2020-09-11 08:50:04 -0500

    Changed height of navbar; edited some CSSfor a fix on the responsiveness issues surrounding the icons in Hobbies and Public Profiles as well as the Publications article left and right padding; added a container div for the Public Profiles icons to see if it helps fix the freeCodeCamp flame vertical stretching issue

--- client/src/PublicProfiles.js
+++ client/src/PublicProfiles.js
@@ -4,12 +4,18 @@ export default function Hobbies() {
 
     return (
         <div className='publicProfiles'>
-            <a id="github" className="profileLink" href="https://github.com/boutchersj" target="_blank" rel="noopener noreferrer" alt="GitHub profile link">
-                <div className="profileLinkDiv">GitHub Logo Image</div>
-            </a>
-            <a id="freecodecamp" className="profileLink" href="https://www.freecodecamp.org/boutchersj" target="_blank" rel="noopener noreferrer" alt="freeCodeCamp profile link">
-                <div className="profileLinkDiv">freeCodeCamp Logo Image</div>
-            </a>
+            <div className="publicProfileDiv">
+                <a id="github" className="profileLink" href="https://github.com/boutchersj" target="_blank" rel="noopener noreferrer" alt="GitHub profile link">
+                    <div className="profileLinkDiv">GitHub Logo Image</div>
+                </a>
+            </div>
+
+            <div className="publicProfileDiv">
+                <a id="freecodecamp" className="profileLink" href="https://www.freecodecamp.org/boutchersj" target="_blank" rel="noopener noreferrer" alt="freeCodeCamp profile link">
+                    <div className="profileLinkDiv">freeCodeCamp Logo Image</div>
+                </a>
+            </div>
+
         </div>
     )
 }
