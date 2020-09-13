eb16805dd042ac430c241cb7eedd0d40a1044466
Steven <boutchersj@gmail.com>
2020-09-13 08:42:35 -0500

    Added form validation for empty submissions, just a required boolean on all inputs

--- client/src/ContactForm.js
+++ client/src/ContactForm.js
@@ -6,9 +6,9 @@ export default function ContactForm() {
             <h2 className="form-title">Reach me about projects & opportunities...</h2>
             <div className="form-container">
                 <form action="https://formspree.io/boutchersj@gmail.com" method="POST">
-                    <input id="name" className="input-name" type="text" name="name" placeholder="Name" />
-                    <input id="email" className="input-email" type="email" name="email" placeholder="Email" />
-                    <textarea id="message" className="input-message" name="message" placeholder="Write a message..." />
+                    <input id="name" className="input-name" type="text" name="name" placeholder="Name" required />
+                    <input id="email" className="input-email" type="email" name="email" placeholder="Email" required />
+                    <textarea id="message" className="input-message" name="message" placeholder="Write a message..." required />
                     <button id="submit" className="btn-submit" type="submit" name="submit">Send message</button>
                 </form>
             </div>
