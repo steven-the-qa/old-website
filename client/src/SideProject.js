5d4a20348697a27d8f69bc38243dee6b2f2a19d1
Steven Boutcher <boutchersj@gmail.com>
2021-03-23 08:53:12 -0500

    Add SideProject and SideProjects components to reduce the effort required to add a new side project to my website

---
+++ client/src/SideProject.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import projectData from './sideProjects.json'
+
+export default function SideProject(props) {
+    const [cardSide, setCardSide] = useState('front');
+    const flipCard = () => {
+        cardSide === 'front' ? setCardSide('reverse') : setCardSide('front')
+    }
+    const FrontSide = () => {
+        return(
+            <div id={props.projectId} className='sideProject' onClick={flipCard}>
+                {projectData[props.projectId].title}
+            </div>
+        )
+    }
+    const features = projectData[props.projectId].features.map(feature => {
+        return (
+            <li className='sideProject-feature'>{feature}</li>
+        )
+    })
+
+    const liveSite = projectData[props.projectId].live && <a className='btn-viewProj' href={projectData[props.projectId].live} target="_blank" rel="noopener noreferrer">View Live</a>
+    
+    const ReverseSide = () => {
+        return (
+            <div id={`${props.projectId}-description`} className='sideProject-description' onClick={flipCard}>
+                <h2>{projectData[props.projectId].title}</h2>
+                <strong>Description:</strong>
+                <p>{projectData[props.projectId].description}</p>
+                <strong>Features:</strong>
+                <ul>{features}</ul>
+                <a className='btn-viewProj' href={projectData[props.projectId].repo} target="_blank" rel="noopener noreferrer">View Repo</a>
+                {liveSite}
+            </div>
+        )
+    }
+
+    return(
+        <div className='sideProjects'>
+            <h3 className='sideProjects-title'>Side Projects</h3>
+            {cardSide === 'front' ? <FrontSide /> : <ReverseSide />}
+        </div>
+    )
+    
+}
---
+++ client/src/SideProjects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SideProject from './SideProject'
+
+export default function SideProjects() {
+    return(
+        <div>
+            <SideProject projectId="medicareandmore" />
+            <SideProject projectId="rubycalculator" />
+        </div>
+    )
+}
