88374a7352006a144930e87e407e528c7833151e
Steven Boutcher <boutchersj@gmail.com>
2021-04-23 16:05:34 -0500

    Add updated resume; revise side project descriptions

--- client/src/Resume.js
+++ client/src/Resume.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import resume from './Steven_Boutcher_Resume_9_14_2020.pdf'
+import resume from './Steven_Boutcher_Resume_4_23_2021.pdf'
 
 export default function Resume() {
     return(
