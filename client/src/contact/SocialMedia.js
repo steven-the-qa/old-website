dcf8349bb543e85e1546e28c8c4df3590a056c1f
Steven Boutcher <boutchersj@gmail.com>
2021-05-29 16:48:31 -0500

    make sure all links open in a new tab

--- client/src/contact/SocialMedia.js
+++ client/src/contact/SocialMedia.js
@@ -5,10 +5,10 @@ export default function SocialMedia() {
         <div className="social-media">
             <h2>...or see what I'm up to!</h2>
             <div className="social-link-container">
-                <a id="linkedIn" className="social-link" href="https://www.linkedin.com/in/boutchersj/" alt="LinkedIn profile link">
+                <a id="linkedIn" className="social-link" href="https://www.linkedin.com/in/boutchersj/" target="_blank" rel="noopener noreferrer" alt="LinkedIn profile link">
                 <div className="social-link-div">LinkedIn logo image</div>
                 </a>
-                <a id="twitter" className="social-link" href="https://twitter.com/StevenBoutcher" alt="Twitter profile link">
+                <a id="twitter" className="social-link" href="https://twitter.com/StevenBoutcher" target="_blank" rel="noopener noreferrer" alt="Twitter profile link">
                     <div className="social-link-div">Twitter logo image</div>
                 </a>
             </div>
