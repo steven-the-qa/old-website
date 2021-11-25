6bf2874c19efa55bbc544f90d533cb3350ba5087
Steven <boutchersj@gmail.com>
2021-11-24 21:53:49 -0600

    refactor site to be front-end only

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css';
+import Landing from './landing/Landing'
+import About from './about/About'
+import Projects from './projects/Projects'
+import Contact from './contact/Contact'
+
+export default function App() {
+
+    return (
+        <Router>   
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/' element={<Landing />} />
+          </Routes>
+        </Router>
+    );
+}
