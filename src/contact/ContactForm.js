9bd777b8b328e9ce5dcba4912b8df078ce84bd6f
Steven Boutcher <boutchersj@gmail.com>
2021-12-28 17:28:47 -0600

    [SOURCE-CODE] Replace formspree integration with EmailJS

--- src/contact/ContactForm.js
+++ src/contact/ContactForm.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 export default function ContactForm() {
-    return (
-        <div className="contact-form">
-            <h2 className="form-title">Let's have a chat over coffee!</h2>
-            <div className="form-container">
-                <form action="https://formspree.io/boutchersj@gmail.com" method="POST">
-                    <input id="name" className="input-name" type="text" name="name" placeholder="Name" required />
-                    <input id="email" className="input-email" type="email" name="email" placeholder="Email" required />
-                    <textarea id="message" className="input-message" name="message" placeholder="Write a message..." required />
-                    <button id="submit" className="btn-submit" type="submit" name="submit">Send message</button>
-                </form>
-            </div>
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    // Use the correct serviceId, templateId, and userId to integrate with EmailJS
+    // emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_USER_ID')
+    emailjs.sendForm('service_im24tc9', 'template_0pa97xj', form.current, 'user_bn196K5WutRKWYHpM0Dv3')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <div className="contact-form">
+        <h2 className="form-title">Let's have a chat over coffee!</h2>
+        <div className="form-container">
+            <form ref={form} onSubmit={sendEmail}>
+                <input type="text" name="from_name" placeholder='Name'/>
+                <input type="email" name="reply_to" placeholder='Email'/>
+                <textarea className='input-message' name="message" placeholder='Write a message...'/>
+                <input id='submit' type="submit" value="Send message" />
+            </form>
         </div>
-    )
-}
+    </div>
+  );
+};
