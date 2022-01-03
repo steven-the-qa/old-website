32c6786a8f124915cf06a514137616a2acdf9362
Steven Boutcher <boutchersj@gmail.com>
2022-01-03 13:07:43 -0600

    [SOURCE-CODE] Update birthday helper project showcase

--- src/projects/SideProjects.js
+++ src/projects/SideProjects.js
@@ -7,7 +7,7 @@ export default function SideProjects() {
             <h3 className='sideProjects-title'>Side Projects</h3>
             <SideProject projectId="medicareandmore" />
             <SideProject projectId="rubycalculator" />
-            <SideProject projectId="birthdayassistant" />
+            <SideProject projectId="birthdayhelper" />
         </div>
     )
 }
