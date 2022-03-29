a7464e533d387b2e1ba9809b02d09d7147284f5a
Steven Boutcher <boutchersj@gmail.com>
2022-03-29 16:47:59 -0500

    [MAIN] Remove Twitter from website

--- src/contact/SocialMedia.js
+++ src/contact/SocialMedia.js
@@ -6,10 +6,7 @@ export default function SocialMedia() {
             <h2>Follow me!</h2>
             <div className="social-link-container">
                 <a id="linkedIn" className="social-link" href="https://www.linkedin.com/in/boutchersj/" target="_blank" rel="noopener noreferrer" alt="LinkedIn profile link">
-                <div className="social-link-div">LinkedIn logo image</div>
-                </a>
-                <a id="twitter" className="social-link" href="https://twitter.com/StevenBoutcher" target="_blank" rel="noopener noreferrer" alt="Twitter profile link">
-                    <div className="social-link-div">Twitter logo image</div>
+                    <div className="social-link-div">LinkedIn logo image</div>
                 </a>
                 <a id="polywork" className="social-link" href="https://www.polywork.com/boutchersj" target="_blank" rel="noopener noreferrer" alt="Polywork profile link">
                     <div className="social-link-div">Polywork logo image</div>
