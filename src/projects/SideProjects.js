69e83f7788ae39c006b4ff19cc83119041be5120
Steven Boutcher <boutchersj@gmail.com>
2022-05-05 08:48:09 -0500

    [MAIN] Add the CSS that uses the new URL variables; refactor Publications to use the SideProject component for each publication

--- src/projects/SideProjects.js
+++ src/projects/SideProjects.js
@@ -5,11 +5,10 @@ export default function SideProjects() {
     return(
         <div className="sideProjects">
             <h3 className='sideProjects-title'>Side Projects</h3>
+            <SideProject projectId="sitefigma" />
             <SideProject projectId="medicaremore" />
             <SideProject projectId="rubycalculator" />
             <SideProject projectId="birthdayhelper" />
-            <SideProject projectId="sitefigma" />
-            <SideProject projectId="scrimbasetstate" />
         </div>
     )
 }
